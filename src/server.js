4ffdbd246de472bbf78b1f0c804f5bb9085d41c5
Anastasia Zamkovskaya <azm@apicosoft.ru>
2021-10-01 11:57:05 +0300

    handlers and multer

--- src/server.js
+++ src/server.js
@@ -1,9 +1,30 @@
 const express = require('express');
+const multer = require('multer');
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Okey');
+});
+
+app.post(
+  '/upload',
+  multer({ dest: './db/images' }).single('image'),
+  (req, res) => {
+    res.send(req.file.filename);
+  }
+);
+
+app.get('/list', (req, res) => {
+  res.send('list');
+});
+
+app.get('/image/:id', (req, res) => {
+  res.send(req.params.id);
+});
+
+app.delete('/image/:id', (req, res) => {
+  res.send(req.params.id);
 });
 
 const PORT = 8080;
